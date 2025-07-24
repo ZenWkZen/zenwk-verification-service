814c1a4c3baafbe484a54f3a515c00a8512d1645
calegria <calegria@MacBook-Pro-de-calegria.local>
2025-07-24 11:32:31 -0500

    V_24/07/2025
    V_24/07/2025

--- zenwk-verification/src/main/java/com/alineumsoft/zenkw/verification/common/exception/enums/CoreExceptionEnum.java
+++ zenwk-verification/src/main/java/com/alineumsoft/zenkw/verification/common/exception/enums/CoreExceptionEnum.java
@@ -2,12 +2,10 @@ package com.alineumsoft.zenkw.verification.common.exception.enums;
 
 import com.alineumsoft.zenkw.verification.common.constants.CommonMessageConstants;
 import com.alineumsoft.zenkw.verification.common.message.component.MessageSourceAccessorComponent;
-
 import lombok.Getter;
 
 /**
- * <b>Enum que define las excepciones tecnicas y funcionales a nivel general del
- * sistema.</b>
+ * <b>Enum que define las excepciones tecnicas y funcionales a nivel general del sistema.</b>
  * 
  * @author <a href="mailto:alineumsoft@gmail.com">C. Alegria</a>
  * @project SecurityUser
@@ -15,84 +13,89 @@ import lombok.Getter;
  */
 @Getter
 public enum CoreExceptionEnum {
-	TECH_COMMON_HISTORICAL_ENTITY_NOT_FOUND("TECH_GEN_001", "common.exception.error.historical.entity.notfound"),
-	TECH_COMMON_MESSAGE_NOT_FOUND("TECH_GEN_002", "common.exception.error.enum.message.notfound"),
-	TECH_COMMON_ANOTATION_ENTITY_NOT_EXISTS("TECH_GEN_003", "common.exception.entity.noexists"),
-	FUNC_COMMON_ERROR_GENERAL("FUNC_COM_01","common.exception.error.general"),
-	FUNC_COMMON_ROLE_NOT_EXIST("FUNC_COM_02","common.exception.rol.noexists");
-	/**
-	 * code
-	 */
-	private final String code;
-	/**
-	 * messageKey
-	 */
-	private final String messageKey;
+  /**
+   * Excepcionoes generales
+   */
+  TECH_COMMON_HISTORICAL_ENTITY_NOT_FOUND("TECH_GEN_0001",
+      "common.exception.error.historical.entity.notfound"), TECH_COMMON_MESSAGE_NOT_FOUND(
+          "TECH_GEN_0002",
+          "common.exception.error.enum.message.notfound"), TECH_COMMON_ANOTATION_ENTITY_NOT_EXISTS(
+              "TECH_GEN_0003", "common.exception.entity.noexists"), FUNC_COMMON_ERROR_GENERAL(
+                  "FUNC_COM_0004", "common.exception.error.general"), FUNC_COMMON_ROLE_NOT_EXIST(
+                      "FUNC_COM_0005", "common.exception.rol.noexists");
 
-	/**
-	 * @param code
-	 * @param messageKey
-	 */
-	CoreExceptionEnum(String code, String messageKey) {
-		this.code = code;
-		this.messageKey = messageKey;
-	}
-	
-	/**
-	 * @return
-	 */
-	public String getMessage() {
-		try {
-			return MessageSourceAccessorComponent.getMessage(messageKey);
-		} catch (Exception e) {
-			throw new RuntimeException(CoreExceptionEnum.TECH_COMMON_MESSAGE_NOT_FOUND.getCodeMessage());
-		}
-	}
+  /**
+   * code
+   */
+  private final String code;
+  /**
+   * messageKey
+   */
+  private final String messageKey;
 
-	/**
-	 * <p>
-	 * <b> General core exception: </b> Obtiene un mensaje formateado basado en una
-	 * clave de mensaje y parámetros opcionales.
-	 * </p>
-	 * 
-	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
-	 * @param params
-	 * @return
-	 */
-	public String getMessage(String... params) {
-		try {
-			return MessageSourceAccessorComponent.getMessage(messageKey, params);
-		} catch (Exception e) {
-			throw new RuntimeException(CoreExceptionEnum.TECH_COMMON_MESSAGE_NOT_FOUND.getCodeMessage(messageKey));
-		}
-	}
+  /**
+   * @param code
+   * @param messageKey
+   */
+  CoreExceptionEnum(String code, String messageKey) {
+    this.code = code;
+    this.messageKey = messageKey;
+  }
 
-	/**
-	 * 
-	 * <p>
-	 * <b> General core exception: </b> Descripción con codigo de la excepcion
-	 * generada
-	 * </p>
-	 * 
-	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
-	 * @return
-	 */
-	public String getCodeMessage() {
-		return String.format(CommonMessageConstants.FORMAT_EXCEPTION, getCode(), getMessage());
-	}
+  /**
+   * @return
+   */
+  public String getMessage() {
+    try {
+      return MessageSourceAccessorComponent.getMessage(messageKey);
+    } catch (Exception e) {
+      throw new RuntimeException(CoreExceptionEnum.TECH_COMMON_MESSAGE_NOT_FOUND.getCodeMessage());
+    }
+  }
 
-	/**
-	 * 
-	 * <p>
-	 * <b> General core exception: </b> Descripción con codigo de la excepcion
-	 * generada con parametros
-	 * </p>
-	 * 
-	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
-	 * @return
-	 */
-	public String getCodeMessage(String... params) {
-		return String.format(CommonMessageConstants.FORMAT_EXCEPTION, getCode(), getMessage(params));
-	}
+  /**
+   * <p>
+   * <b> General core exception: </b> Obtiene un mensaje formateado basado en una clave de mensaje y
+   * parámetros opcionales.
+   * </p>
+   * 
+   * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+   * @param params
+   * @return
+   */
+  public String getMessage(String... params) {
+    try {
+      return MessageSourceAccessorComponent.getMessage(messageKey, params);
+    } catch (Exception e) {
+      throw new RuntimeException(
+          CoreExceptionEnum.TECH_COMMON_MESSAGE_NOT_FOUND.getCodeMessage(messageKey));
+    }
+  }
+
+  /**
+   * 
+   * <p>
+   * <b> General core exception: </b> Descripción con codigo de la excepcion generada
+   * </p>
+   * 
+   * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+   * @return
+   */
+  public String getCodeMessage() {
+    return String.format(CommonMessageConstants.FORMAT_EXCEPTION, getCode(), getMessage());
+  }
+
+  /**
+   * 
+   * <p>
+   * <b> General core exception: </b> Descripción con codigo de la excepcion generada con parametros
+   * </p>
+   * 
+   * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+   * @return
+   */
+  public String getCodeMessage(String... params) {
+    return String.format(CommonMessageConstants.FORMAT_EXCEPTION, getCode(), getMessage(params));
+  }
 
 }
--- zenwk-verification/src/main/java/com/alineumsoft/zenkw/verification/controller/VerificationController.java
+++ zenwk-verification/src/main/java/com/alineumsoft/zenkw/verification/controller/VerificationController.java
@@ -2,15 +2,14 @@ package com.alineumsoft.zenkw.verification.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.alineumsoft.zenkw.verification.dto.TokenDTO;
 import com.alineumsoft.zenkw.verification.service.VerificationService;
-
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * 
  * <p>
- * Controlador para la gestión de tokens de verificación de usuario. Permite
- * enviar y validar códigos de verificación vía correo electrónico.
+ * Controlador para la gestión de tokens de verificación de usuario. Permite enviar y validar
+ * códigos de verificación vía correo electrónico.
  * </p>
  * 
  * @author <a href="mailto:alineumsoft@gmail.com">C. Alegria</a>
@@ -28,50 +27,52 @@ import lombok.extern.slf4j.Slf4j;
  */
 @RestController
 @RequestMapping("/api/verification/token")
+@CrossOrigin(origins = "*")
 @Slf4j
 @RequiredArgsConstructor
 public class VerificationController {
 
-	/**
-	 * Email producer service
-	 */
-	private final VerificationService verificationService;
+  /**
+   * Email producer service
+   */
+  private final VerificationService verificationService;
 
-	/**
-	 * <p>
-	 * <b> CU003_Gestionar token de verificación. </b> Envía un token de
-	 * verificación a la cuenta de correo del usuario.
-	 * </p>
-	 * 
-	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
-	 * @param request
-	 * @param dto
-	 * @param userDetails
-	 * @return
-	 */
-	@PostMapping("/send")
-	public ResponseEntity<Void> sendToken(HttpServletRequest request, @Validated @RequestBody TokenDTO dto) {
-		// Enviar solicitud de correo a la cola de RabbitMQ
-		verificationService.sendToken(dto, request);
-		return ResponseEntity.noContent().build();
-	}
+  /**
+   * <p>
+   * <b> CU003_Gestionar token de verificación. </b> Envía un token de verificación a la cuenta de
+   * correo del usuario.
+   * </p>
+   * 
+   * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+   * @param request
+   * @param dto
+   * @param userDetails
+   * @return
+   */
+  @PostMapping("/send")
+  public ResponseEntity<Void> sendToken(HttpServletRequest request,
+      @Validated @RequestBody TokenDTO dto) {
+    // Enviar solicitud de correo a la cola de RabbitMQ
+    verificationService.sendToken(dto, request);
+    return ResponseEntity.noContent().build();
+  }
 
-	/**
-	 * <p>
-	 * <b> CU003_Gestionar token de verificación. </b> Valida un código de
-	 * verificación generado previamente.
-	 * </p>
-	 * 
-	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
-	 * @param request
-	 * @param code
-	 * @param dto
-	 * @param userDetails
-	 * @return
-	 */
-	@PostMapping("/validate/{code}")
-	public ResponseEntity<Boolean> verifyToken(HttpServletRequest request, @PathVariable String code,
-			@Validated @RequestBody TokenDTO dto) {
-		return ResponseEntity.ok(verificationService.verifyToken(code, dto, request));
-	}
+  /**
+   * <p>
+   * <b> CU003_Gestionar token de verificación. </b> Valida un código de verificación generado
+   * previamente.
+   * </p>
+   * 
+   * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+   * @param request
+   * @param code
+   * @param dto
+   * @param userDetails
+   * @return
+   */
+  @PostMapping("/validate/{code}")
+  public ResponseEntity<Boolean> verifyToken(HttpServletRequest request, @PathVariable String code,
+      @Validated @RequestBody TokenDTO dto) {
+    return ResponseEntity.ok(verificationService.verifyToken(code, dto, request));
+  }
 }

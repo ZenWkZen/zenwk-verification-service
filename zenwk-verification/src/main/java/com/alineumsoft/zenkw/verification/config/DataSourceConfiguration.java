0d64708712e98c29067b721f7e5b609c27fa5a2b
calegria <calegria@MacBook-Pro-de-calegria.local>
2025-11-10 17:22:14 -0500

    [config][pu-git-actions][V02]: Creación configuración perfil test.

--- zenwk-verification/src/main/java/com/alineumsoft/zenkw/verification/config/DataSourceConfiguration.java
+++ zenwk-verification/src/main/java/com/alineumsoft/zenkw/verification/config/DataSourceConfiguration.java
@@ -6,37 +6,39 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration(proxyBeanMethods = false)
+@Profile({"default", "dev", "local"})
 public class DataSourceConfiguration {
-	/**
-	 * <p>
-	 * <b> CU001_Seguridad_Creacion_Usuario </b> Clase configuracion de datasource
-	 * </p>
-	 *
-	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
-	 * @return
-	 */
-	@Bean
-	@Primary
-	@ConfigurationProperties("app.datasource")
-	public DataSourceProperties dataSourceProperties() {
-		return new DataSourceProperties();
-	}
+  /**
+   * <p>
+   * <b> CU001_Seguridad_Creacion_Usuario </b> Clase configuracion de datasource
+   * </p>
+   *
+   * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+   * @return
+   */
+  @Bean
+  @Primary
+  @ConfigurationProperties("app.datasource")
+  public DataSourceProperties dataSourceProperties() {
+    return new DataSourceProperties();
+  }
 
-	/**
-	 * <p>
-	 * <b> CU001_Seguridad_Creacion_Usuario </b> Configuracion pool de conexiones
-	 * </p>
-	 *
-	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
-	 * @return
-	 */
-	@Bean
-	@ConfigurationProperties("app.datasource.hikari")
-	public HikariDataSource dataSource() {
-		return DataSourceBuilder.create().type(HikariDataSource.class).build();
-	}
+  /**
+   * <p>
+   * <b> CU001_Seguridad_Creacion_Usuario </b> Configuracion pool de conexiones
+   * </p>
+   *
+   * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+   * @return
+   */
+  @Bean
+  @ConfigurationProperties("app.datasource.hikari")
+  public HikariDataSource dataSource() {
+    return DataSourceBuilder.create().type(HikariDataSource.class).build();
+  }
 
 }
